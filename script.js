31fdd19174e31bcb98ec359f65e2b1dc4e9ef5b4
Daniel <dankarp1999@gmail.com>
2022-05-01 15:37:39 -0400

    Add playRound function to determine the winner

--- script.js
+++ script.js
@@ -8,3 +8,44 @@ function computerPlay() {
 		return 'Scissors';
 	}
 }
+
+function playRound(playerSelection, computerSelection) {
+	if (playerSelection === 'Rock') {
+		if (computerSelection === 'Paper') {
+			return 'You lose, Paper beats Rock';
+		} else if (computerSelection === 'Scissors') {
+			return 'You win, Rock beats Scissors';
+		} else {
+			return "It's a draw";
+		}
+	} else if (playerSelection === 'Paper') {
+		if (computerSelection === 'Scissors') {
+			return 'You lose, Scissors beats Paper';
+		} else if (computerSelection === 'Rock') {
+			return 'You win, Paper beats Rock';
+		} else {
+			return "It's a draw";
+		}
+	} else if (playerSelection === 'Scissors') {
+		if (computerSelection === 'Rock') {
+			return 'You lose, Rock beats Scissors';
+		} else if (computerSelection === 'Paper') {
+			return 'You win, Scissors beats Paper';
+		} else {
+			return "It's a draw";
+		}
+	}
+}
+function game() {
+	for (let i = 0; i <= 5; i++) {
+		playRound(prompt('Choose your weapon!'), computerPlay);
+	}
+}
+
+// if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+//     return 'You win! Rock beats Scissors!';
+// } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+//     return `You Lose! Paper beats Rock!`;
+// } else if (playerSelection === 'Rock' && computerSelection === 'Rock') {
+//     return `It's a tie!`;
+//
