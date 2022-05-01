191ab756e6a2cafea4523b8fe5a56db451adfed9
Daniel <dankarp1999@gmail.com>
2022-05-01 16:56:04 -0400

    Add score keeping to the game() function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay() {
 	let num = Math.floor(Math.random() * 3) + 1;
 	if (num === 1) {
@@ -12,24 +15,30 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
 	if (playerSelection.toUpperCase() === 'ROCK') {
 		if (computerSelection === 'PAPER') {
+			computerScore++;
 			return 'You lose, Paper beats Rock';
 		} else if (computerSelection === 'SCISSORS') {
+			playerScore++;
 			return 'You win, Rock beats Scissors';
 		} else {
 			return "It's a draw";
 		}
 	} else if (playerSelection.toUpperCase() === 'PAPER') {
 		if (computerSelection === 'SCISSORS') {
+			computerScore++;
 			return 'You lose, Scissors beats Paper';
 		} else if (computerSelection === 'ROCK') {
+			playerScore++;
 			return 'You win, Paper beats Rock';
 		} else {
 			return "It's a draw";
 		}
 	} else if (playerSelection.toUpperCase() === 'SCISSORS') {
 		if (computerSelection === 'ROCK') {
+			computerScore++;
 			return 'You lose, Rock beats Scissors';
 		} else if (computerSelection === 'PAPER') {
+			playerScore++;
 			return 'You win, Scissors beats Paper';
 		} else {
 			return "It's a draw";
@@ -39,6 +48,17 @@ function playRound(playerSelection, computerSelection) {
 function game() {
 	for (let i = 0; i < 5; i++) {
 		console.log(playRound(prompt(), computerPlay()));
+	}
+	if (playerScore > computerScore) {
+		console.log(
+			`The score is ${playerScore} : ${computerScore}, You are the WINNER!`
+		);
+	} else if (playerScore < computerScore) {
+		console.log(
+			`The score is ${playerScore} : ${computerScore}, You LOST!:(`
+		);
+	} else {
+		console.log('Congrats, you tied');
 	}
 }
 
