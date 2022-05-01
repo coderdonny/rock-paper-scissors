26e04ff6cfdb7218ae4760e0fb20ac3a4f86bc8c
Daniel <dankarp1999@gmail.com>
2022-05-01 14:07:08 -0400

    Create computerPlay function to allow computer to pick Rock, Paper or Scissors

---
+++ script.js
@@ -0,0 +1,10 @@
+function computerPlay() {
+	let num = Math.floor(Math.random() * 3) + 1;
+	if (num === 1) {
+		return 'Rock';
+	} else if (num === 2) {
+		return 'Paper';
+	} else {
+		return 'Scissors';
+	}
+}
