eadeb57e4f63ea0d43164f8280090b7d496ab959
Daniel <dankarp1999@gmail.com>
2022-05-05 20:39:27 -0400

    Style images, add counter prototype and update to google fonts

--- script.js
+++ script.js
@@ -1,81 +1,99 @@
 //initializes the scores for the player and computer
 let playerScore = 0;
 let computerScore = 0;
+let round = 1;
+
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
 
 //generates a random number and is assigned to either rock, paper or scissors
 function computerPlay() {
 	let num = Math.floor(Math.random() * 3) + 1;
 	if (num === 1) {
-		return 'ROCK';
+		return 'rock';
 	} else if (num === 2) {
-		return 'PAPER';
+		return 'paper';
 	} else {
-		return 'SCISSORS';
+		return 'scissors';
 	}
 }
 
 //once the player (and computer) makes their choice, this function determines the winner
 function playRound(playerSelection, computerSelection) {
-	if (
-		!(
-			playerSelection.toUpperCase() === 'ROCK' ||
-			playerSelection.toUpperCase() === 'PAPER' ||
-			playerSelection.toUpperCase() === 'SCISSORS'
-		)
-	) {
-		return 'Selection Error';
-	} else {
-		if (playerSelection.toUpperCase() === 'ROCK') {
-			if (computerSelection === 'PAPER') {
-				computerScore++;
-				return 'You lose, Paper beats Rock';
-			} else if (computerSelection === 'SCISSORS') {
-				playerScore++;
-				return 'You win, Rock beats Scissors';
-			} else {
-				return "It's a draw";
-			}
-		} else if (playerSelection.toUpperCase() === 'PAPER') {
-			if (computerSelection === 'SCISSORS') {
-				computerScore++;
-				return 'You lose, Scissors beats Paper';
-			} else if (computerSelection === 'ROCK') {
-				playerScore++;
-				return 'You win, Paper beats Rock';
-			} else {
-				return "It's a draw";
-			}
-		} else if (playerSelection.toUpperCase() === 'SCISSORS') {
-			if (computerSelection === 'ROCK') {
-				computerScore++;
-				return 'You lose, Rock beats Scissors';
-			} else if (computerSelection === 'PAPER') {
-				playerScore++;
-				return 'You win, Scissors beats Paper';
-			} else {
-				return "It's a draw";
-			}
+	if (playerSelection === 'rock') {
+		if (computerSelection === 'paper') {
+			computerScore++;
+			round++;
+			return 'You lose, Paper beats Rock';
+		} else if (computerSelection === 'scissors') {
+			playerScore++;
+			round++;
+			return 'You win, Rock beats Scissors';
+		} else {
+			round++;
+			return "It's a draw";
+		}
+	} else if (playerSelection === 'paper') {
+		if (computerSelection === 'scissors') {
+			computerScore++;
+			round++;
+			return 'You lose, Scissors beats Paper';
+		} else if (computerSelection === 'rock') {
+			playerScore++;
+			round++;
+			return 'You win, Paper beats Rock';
+		} else {
+			round++;
+			return "It's a draw";
+		}
+	} else if (playerSelection === 'scissors') {
+		if (computerSelection === 'rock') {
+			computerScore++;
+			round++;
+			return 'You lose, Rock beats Scissors';
+		} else if (computerSelection === 'paper') {
+			playerScore++;
+			round++;
+			return 'You win, Scissors beats Paper';
+		} else {
+			round++;
+			return "It's a draw";
 		}
 	}
 }
 
-//runs the playRound() functions 5 times and determines the winner
-function game() {
-	for (let i = 0; i < 5; i++) {
-		console.log(playRound(prompt(), computerPlay()));
-	}
-	if (playerScore > computerScore) {
-		console.log(
-			`The score is ${playerScore} : ${computerScore}, You are the WINNER!`
-		);
-	} else if (playerScore < computerScore) {
-		console.log(
-			`The score is ${playerScore} : ${computerScore}, You LOST!:(`
-		);
-	} else {
-		console.log('Congrats, you tied.');
-	}
+rock.addEventListener('click', () => {
+	Click('rock');
+});
+paper.addEventListener('click', () => {
+	Click('paper');
+});
+scissors.addEventListener('click', () => {
+	Click('scissors');
+});
+
+function Click(playerSelection) {
+	const computerSelection = computerPlay();
+	console.log(playRound(playerSelection, computerSelection));
+	winner();
 }
 
-//initializes the games
-// game();
+function winner() {
+	if (round === 6)
+		if (playerScore > computerScore) {
+			alert('Winner');
+			reset();
+		} else if (playerScore < computerScore) {
+			alert('Loser');
+			reset();
+		} else {
+			alert('tied');
+		}
+}
+
+function reset() {
+	round = 0;
+	playerScore = 0;
+	computerScore = 0;
+}
